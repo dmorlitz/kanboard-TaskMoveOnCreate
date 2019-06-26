c00dc1ea7e63f96b381c72b1c3275aa6ab69510c
David Morlitz <david@morlitz.com>
2019-06-26 11:41:17 -0400

    First commit

---
+++ Plugin.php
@@ -0,0 +1,40 @@
+<?php
+namespace Kanboard\Plugin\TaskMoveOnCreate;
+use Kanboard\Core\Plugin\Base;
+use Kanboard\Plugin\TaskMoveOnCreate\Action\TaskMoveOnCreate;
+class Plugin extends Base
+{
+    public function initialize()
+    {
+        $this->actionManager->register(new TaskMoveOnCreate($this->container));
+    }
+    public function getPluginName()
+    {
+        return 'TaskMoveOnCreate';
+    }
+
+    public function getPluginDescription()
+    {
+        return t('Automatically move to a column on task creation');
+    }
+
+    public function getPluginAuthor()
+    {
+        return 'David Morlitz';
+    }
+
+    public function getPluginVersion()
+    {
+        return '0.0.1';
+    }
+
+    public function getPluginHomepage()
+    {
+        return 'https://github.com/dmorlitz/kanboard-TaskMoveOnCreate';
+    }
+
+    public function getCompatibleVersion()
+    {
+        return '>=1.0.44';
+    }
+}
